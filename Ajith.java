4317ad7523ad85d9ef0496458f4c59440e3f0a78
Ajith219 <attiralaajith@gmail.com>
2025-07-19 09:53:47 +0530

    Add files via upload

---
+++ Ajith.java
@@ -0,0 +1,22 @@
+class Sample
+{
+String name;
+String type;
+void show(String a,String b)
+{
+name=a;
+type=b;
+}
+void print()
+{
+System.out.printf("id=%d\nname=%c",x,name);
+}
+}
+class Const
+{
+public static void main(String args[])
+{
+Constructor c=new Constructor();
+c.print();
+}
+}
